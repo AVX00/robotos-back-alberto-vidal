b7fca499130b26ae4e08421e94185c51d37b8b4d
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-19 17:24:49 +0100

    :ambulance: fixed get by id

--- src/server/routers/robotsRouter.js
+++ src/server/routers/robotsRouter.js
@@ -10,13 +10,12 @@ const tokenValidator = require("../middlewares/validators/tokenValidator");
 
 const router = express.Router();
 
-router.get("/:id", getRobot);
+router.get("/:idRobot?", getRobots, getRobot);
 
-router.get("/", getRobots);
 router.post("/create", tokenValidator, createRobot);
 
 router.put("/update", tokenValidator, updateRobot);
 
-router.delete("/delete/:id", tokenValidator, deleteRobot);
+router.delete("/delete/:idRobot", tokenValidator, deleteRobot);
 
 module.exports = router;
