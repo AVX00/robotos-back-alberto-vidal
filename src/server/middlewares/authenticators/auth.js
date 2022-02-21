22f98ce7e100380f00eb01ba1b1742c2ddfda67d
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-21 23:07:41 +0100

    :sparkles: created auth handler

---
+++ src/server/middlewares/authenticators/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+const auth = async (req, res, next) => {
+  const authHeader = req.header("Authorization");
+
+  if (!authHeader) {
+    const error = new Error("token not provided");
+    error.status(401);
+    next(error);
+  }
+
+  const token = authHeader.replace("Bearer ", "");
+
+  try {
+    const user = await jwt.verify(token, process.env.SECRET);
+    req.userId = user.id;
+  } catch (error) {
+    next(new Error("invalid token"));
+  }
+};
+
+module.exports = auth;
