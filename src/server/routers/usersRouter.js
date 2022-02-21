19ff61455a0ede1683510785349ec00b01cb6076
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-21 23:55:15 +0100

    :recycle: refactored routers into controllers

--- src/server/routers/usersRouter.js
+++ src/server/routers/usersRouter.js
@@ -1,35 +1,8 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
-const argon2 = require("argon2");
-const User = require("../../dataBase/models/User");
+const { loginUser, registerUser } = require("../controllers/userControllers");
 
 const router = express.Router();
 
-router.post("/register", async (req, res, next) => {
-  const { userName, password: userPassword } = req.body;
+router.post("/register", registerUser);
 
-  try {
-    const password = await argon2.hash(userPassword);
-    await User.create({ userName, password });
-  } catch (error) {
-    next(new Error("error creating user"));
-  }
-});
-
-router.post("/login", async (req, res, next) => {
-  const { userName, password: userPassword } = req.body;
-
-  try {
-    const { password, id } = await User.findOne({ userName });
-    if (await argon2.verify(password, userPassword)) {
-      const token = jwt.sign({ userName, id }, process.env.SECRET, {
-        expiresIn: "4d",
-      });
-      res.json({ token });
-    } else {
-      res.status(401).json({ error: "incorrect password" });
-    }
-  } catch (error) {
-    next(error);
-  }
-});
+router.post("/login", loginUser);
