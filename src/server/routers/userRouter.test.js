9a65248c6f83e009b62d501480470f688ce74108
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-22 19:14:54 +0100

    :white_check_mark: tested new user with conflicts

--- src/server/routers/userRouter.test.js
+++ src/server/routers/userRouter.test.js
@@ -11,7 +11,9 @@ beforeAll(async () => {
   dataBase = await MongoMemoryServer.create();
   const uri = dataBase.getUri();
   await connectdb(uri);
-  User.deleteMany({});
+});
+beforeEach(async () => {
+  await User.deleteMany({});
 });
 afterAll(() => {
   mongoose.connection.close();
@@ -22,22 +24,31 @@ describe("Given a users router", () => {
   describe("When it receives a post request at /user/register with body {userName: 'joselito': password:'123'} an username isn't taken", () => {
     test("Then it should respond with status 201 and a json {'user': 'created'}", async () => {
       const expectedResponse = { user: "created" };
+      const expectedStatus = 201;
       const newUser = { userName: "joselito", password: "123" };
 
       const { body } = await request(app)
         .post("/user/register")
         .send(newUser)
-        .expect(201);
+        .expect(expectedStatus);
 
       expect(body).toEqual(expectedResponse);
     });
   });
 
   describe("When it receives a post request at /user/register with body {userName: 'joselito', password: '123' } and username is taken", () => {
-    test("Then ", () => {
-      // Arrange
-      // Act
-      // Assert
+    test("Then it should respond with status 409 and a json {error: 'userName taken'}", async () => {
+      const expectedResponse = { error: "userName taken" };
+      const expectedStatus = 409;
+      const newUser = { userName: "joselito", password: "123" };
+      User.create(newUser);
+
+      const { body } = await request(app)
+        .post("/user/register")
+        .send(newUser)
+        .expect(expectedStatus);
+
+      expect(body).toEqual(expectedResponse);
     });
   });
 });
